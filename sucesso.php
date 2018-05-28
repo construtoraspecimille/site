0a02249d242413e8257fff3d46a16555d9c2bdd8
Marcos <construtorasp@gmail.com>
2018-05-28 16:02:42 -0300

    primeiro

---
+++ sucesso.php
@@ -0,0 +1,9 @@
+<head>
+<title>Construtora Specimille</title>
+</head>
+<body>
+<div style="margin:auto; border-color:gray; border-style:solid; border-width:1px; width:400px; padding:10px">
+<h4 style="color:'blue';">E-mail Enviado com Sucesso!</h4>
+<a href="index.html">Voltar para home...</a>
+</body>
+</html>
